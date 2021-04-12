49c9f4b21d9e3881a49237094328bf204a0310cb
Samin1615088 <samin.shafkat@gmail.com>
2021-04-12 12:11:03 +0600

    partial background is added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from '../Header/Header';
-import HeaderMain from '../HeaderMain.js/HeaderMain';
+
 
 const Home = () => { 
     return (
         <div className="vh-100">
             <Header />
-            <HeaderMain />
         </div>
     );
 };
