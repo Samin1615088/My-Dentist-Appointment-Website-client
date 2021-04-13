7ffec863f52bb40a7c381d602b6f4c34e85e23d2
Samin1615088 <samin.shafkat@gmail.com>
2021-04-14 05:23:52 +0600

    adding Appointment date dynamically and adding fakeData

--- src/components/Appointment/Appointment/Appointment.js
+++ src/components/Appointment/Appointment/Appointment.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
 import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
+import BookAppointment from '../BookAppointment/BookAppointment';
 import './Appointment.css';
 
 const Appointment = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const handleDateChange = (date) => {
         setSelectedDate(date);
+        console.log(selectedDate);
     }
 
     return (
-        <div>
+        <div className="">
             <Navbar />
             <AppointmentHeader handleDateChange={handleDateChange} />
+            <BookAppointment date={selectedDate} />
             <Footer />
         </div>
     );
